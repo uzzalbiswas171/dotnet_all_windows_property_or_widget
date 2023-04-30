8e9b6cb2cb6ab9e141e4f1bb0eeeb8fcaf3e1314
Uzzal Biswas <abir@momen-realestatesbd.com>
2023-04-30 17:42:27 +0600

    all widget are here

---
+++ Combobox/Form1.Designer.cs
@@ -0,0 +1,346 @@
+﻿namespace Combobox
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox = new System.Windows.Forms.ComboBox();
+            this.showname = new System.Windows.Forms.Label();
+            this.Alu = new System.Windows.Forms.CheckBox();
+            this.moric = new System.Windows.Forms.CheckBox();
+            this.potol = new System.Windows.Forms.CheckBox();
+            this.begun = new System.Windows.Forms.CheckBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.SaleSection = new System.Windows.Forms.GroupBox();
+            this.nametextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.addEmplopyeebtn = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.phonetextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.AddlistView = new System.Windows.Forms.ListView();
+            this.Name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Phone = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label3 = new System.Windows.Forms.Label();
+            this.SaleSection.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox
+            // 
+            this.comboBox.FormattingEnabled = true;
+            this.comboBox.Location = new System.Drawing.Point(60, 24);
+            this.comboBox.Name = "comboBox";
+            this.comboBox.Size = new System.Drawing.Size(258, 28);
+            this.comboBox.TabIndex = 0;
+            this.comboBox.SelectedIndexChanged += new System.EventHandler(this.comboBox_SelectedIndexChanged);
+            // 
+            // showname
+            // 
+            this.showname.AutoSize = true;
+            this.showname.Location = new System.Drawing.Point(410, 24);
+            this.showname.Name = "showname";
+            this.showname.Size = new System.Drawing.Size(0, 20);
+            this.showname.TabIndex = 1;
+            // 
+            // Alu
+            // 
+            this.Alu.AutoSize = true;
+            this.Alu.Location = new System.Drawing.Point(60, 79);
+            this.Alu.Name = "Alu";
+            this.Alu.Size = new System.Drawing.Size(58, 24);
+            this.Alu.TabIndex = 2;
+            this.Alu.Text = "Alu";
+            this.Alu.UseVisualStyleBackColor = true;
+            // 
+            // moric
+            // 
+            this.moric.AutoSize = true;
+            this.moric.Location = new System.Drawing.Point(60, 162);
+            this.moric.Name = "moric";
+            this.moric.Size = new System.Drawing.Size(73, 24);
+            this.moric.TabIndex = 3;
+            this.moric.Text = "moric";
+            this.moric.UseVisualStyleBackColor = true;
+            // 
+            // potol
+            // 
+            this.potol.AutoSize = true;
+            this.potol.Location = new System.Drawing.Point(60, 120);
+            this.potol.Name = "potol";
+            this.potol.Size = new System.Drawing.Size(70, 24);
+            this.potol.TabIndex = 4;
+            this.potol.Text = "potol";
+            this.potol.UseVisualStyleBackColor = true;
+            // 
+            // begun
+            // 
+            this.begun.AutoSize = true;
+            this.begun.Location = new System.Drawing.Point(60, 202);
+            this.begun.Name = "begun";
+            this.begun.Size = new System.Drawing.Size(119, 24);
+            this.begun.TabIndex = 5;
+            this.begun.Text = "All Checked";
+            this.begun.UseVisualStyleBackColor = true;
+            this.begun.CheckedChanged += new System.EventHandler(this.begun_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(19, 41);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(126, 24);
+            this.radioButton1.TabIndex = 6;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(19, 83);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(126, 24);
+            this.radioButton2.TabIndex = 7;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(19, 129);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(126, 24);
+            this.radioButton3.TabIndex = 8;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Location = new System.Drawing.Point(19, 170);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(126, 24);
+            this.radioButton4.TabIndex = 9;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "radioButton4";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Location = new System.Drawing.Point(60, 253);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(126, 24);
+            this.radioButton5.TabIndex = 10;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "radioButton5";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Location = new System.Drawing.Point(60, 295);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(126, 24);
+            this.radioButton6.TabIndex = 11;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "radioButton6";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Location = new System.Drawing.Point(60, 338);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(126, 24);
+            this.radioButton7.TabIndex = 12;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "radioButton7";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // SaleSection
+            // 
+            this.SaleSection.Controls.Add(this.radioButton1);
+            this.SaleSection.Controls.Add(this.radioButton2);
+            this.SaleSection.Controls.Add(this.radioButton3);
+            this.SaleSection.Controls.Add(this.radioButton4);
+            this.SaleSection.Location = new System.Drawing.Point(581, 51);
+            this.SaleSection.Name = "SaleSection";
+            this.SaleSection.Size = new System.Drawing.Size(191, 242);
+            this.SaleSection.TabIndex = 13;
+            this.SaleSection.TabStop = false;
+            this.SaleSection.Text = "SaleSection";
+            // 
+            // nametextBox
+            // 
+            this.nametextBox.Location = new System.Drawing.Point(282, 120);
+            this.nametextBox.Name = "nametextBox";
+            this.nametextBox.Size = new System.Drawing.Size(201, 26);
+            this.nametextBox.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(278, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Name";
+            // 
+            // addEmplopyeebtn
+            // 
+            this.addEmplopyeebtn.Location = new System.Drawing.Point(396, 221);
+            this.addEmplopyeebtn.Name = "addEmplopyeebtn";
+            this.addEmplopyeebtn.Size = new System.Drawing.Size(87, 33);
+            this.addEmplopyeebtn.TabIndex = 1;
+            this.addEmplopyeebtn.Text = "Add ";
+            this.addEmplopyeebtn.UseVisualStyleBackColor = true;
+            this.addEmplopyeebtn.Click += new System.EventHandler(this.addEmplopyeebtn_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(572, 12);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 26);
+            this.dateTimePicker1.TabIndex = 16;
+            // 
+            // phonetextBox
+            // 
+            this.phonetextBox.Location = new System.Drawing.Point(282, 180);
+            this.phonetextBox.Name = "phonetextBox";
+            this.phonetextBox.Size = new System.Drawing.Size(201, 26);
+            this.phonetextBox.TabIndex = 17;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(278, 155);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 20);
+            this.label2.TabIndex = 18;
+            this.label2.Text = " ";
+            // 
+            // AddlistView
+            // 
+            this.AddlistView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Name,
+            this.Phone});
+            this.AddlistView.HideSelection = false;
+            this.AddlistView.Location = new System.Drawing.Point(282, 324);
+            this.AddlistView.Name = "AddlistView";
+            this.AddlistView.Size = new System.Drawing.Size(248, 197);
+            this.AddlistView.TabIndex = 19;
+            this.AddlistView.UseCompatibleStateImageBehavior = false;
+            this.AddlistView.View = System.Windows.Forms.View.Details;
+            // 
+            // Name
+            // 
+            this.Name.Text = "Name";
+            // 
+            // Phone
+            // 
+            this.Phone.Text = "Phone";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(278, 155);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 20);
+            this.label3.TabIndex = 20;
+            this.label3.Text = "Phone";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(806, 679);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.AddlistView);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.phonetextBox);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.addEmplopyeebtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.nametextBox);
+            this.Controls.Add(this.SaleSection);
+            this.Controls.Add(this.radioButton7);
+            this.Controls.Add(this.radioButton6);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.begun);
+            this.Controls.Add(this.potol);
+            this.Controls.Add(this.moric);
+            this.Controls.Add(this.Alu);
+            this.Controls.Add(this.showname);
+            this.Controls.Add(this.comboBox);
+           
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.SaleSection.ResumeLayout(false);
+            this.SaleSection.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox;
+        private System.Windows.Forms.Label showname;
+        private System.Windows.Forms.CheckBox Alu;
+        private System.Windows.Forms.CheckBox moric;
+        private System.Windows.Forms.CheckBox potol;
+        private System.Windows.Forms.CheckBox begun;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.GroupBox SaleSection;
+        private System.Windows.Forms.TextBox nametextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button addEmplopyeebtn;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.TextBox phonetextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListView AddlistView;
+        private System.Windows.Forms.ColumnHeader Name;
+        private System.Windows.Forms.ColumnHeader Phone;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Combobox/Form1.cs
@@ -0,0 +1,80 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Combobox
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+ 
+
+        private void Loadcombobox()
+        {
+
+            comboBox.Items.Add("Uzza Biswas");
+            comboBox.Items.Add("Kumar Biswas");
+            comboBox.Items.Add("Bijoy Biswas");
+            comboBox.Items.Add("Ripon Biswas");
+
+             
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Loadcombobox();
+        }
+
+        private void comboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            showname.Text= comboBox.Text.ToString();
+        }
+        bool valu = false;
+        private void begun_CheckedChanged(object sender, EventArgs e)
+
+        {
+            valu =! valu;
+
+            if (valu == true)
+            {
+                Alu.Checked = true;
+                potol.Checked = true;
+                moric.Checked = true;
+            }
+            else {
+                Alu.Checked = false;
+                potol.Checked = false;
+                moric.Checked = false;
+            }
+        }
+
+        private void addEmplopyeebtn_Click(object sender, EventArgs e)
+        {
+            String name=nametextBox.Text;
+            String phone=phonetextBox.Text;
+
+            ListViewItem item=new ListViewItem(name);
+            item.SubItems.Add(phone);
+            AddlistView.Items.Add(item);
+
+
+
+        }
+
+        private void employeeListBox_MouseDoubleClick(object sender, MouseEventArgs e)
+        {
+
+        }
+    }
+}
